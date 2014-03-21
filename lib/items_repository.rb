472cd06ed3e6df5b026208d3d23ae036a3f5013f
Mark Evans <m.evans@{"me"="twenty4play@gmail.com", "et"="evantedesco@gmail.com"}>
2014-03-21 10:37:50 -0600

    created item repository to extract class.
    Signed-off-by: Evan Tedesco <e.tedesco@{"me"="twenty4play@gmail.com", "et"="evantedesco@gmail.com"}>

---
+++ lib/items_repository.rb
@@ -0,0 +1,5 @@
+class ItemsRepository
+  def items
+    [Item.new(2,"item 2")]
+  end
+end
--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-
+require './lib/item'
+require './lib/items_repository'
 get '/' do
   erb :index
 end
